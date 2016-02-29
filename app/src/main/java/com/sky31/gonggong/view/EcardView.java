90bd1a9b008fe64f65832b5148042c54f9127d3d
CasparGX <630248976@qq.com>
2016-02-29 17:31:58 +0800

    Init Commit

---
+++ app/src/main/java/com/sky31/gonggong/view/EcardView.java
@@ -0,0 +1,12 @@
+package com.sky31.gonggong.view;
+
+import com.sky31.gonggong.model.EcardModel;
+import com.sky31.gonggong.model.StudentInfoModel;
+
+/**
+ * Created by root on 16-2-29.
+ */
+public interface EcardView {
+    void getBalance(EcardModel ecardModel);
+
+}
