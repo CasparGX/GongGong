94d5ae6a282a21ba5c1d16c0bc0c058e095647a8
wukunguang <wukunguang@magicguang.lan>
2016-03-01 22:18:16 +0800

    ..

--- app/src/main/java/com/sky31/gonggong/model/SwzlService.java
+++ app/src/main/java/com/sky31/gonggong/model/SwzlService.java
@@ -5,7 +5,6 @@ import com.sky31.gonggong.config.Constants;
 import retrofit.Call;
 import retrofit.http.Body;
 import retrofit.http.POST;
-import retrofit.http.Query;
 
 /**
  * Created by wukunguang on 16-3-1.
@@ -13,7 +12,6 @@ import retrofit.http.Query;
 public interface SwzlService {
 
     String submitLostParm = "ask_method="+Constants.Value.SWZL_ASK_METHOD_JSON+"&"+"action="+Constants.Value.SWZL_SUBMIT_LOST;
-
     @POST(Constants.Api.SWZL_ACTION+"?"+submitLostParm)
     Call<SwzlResModel> getSubResult(@Body LostAndFoundModel model);
 
