38bd37a2c28869afdcec970726f1f7f955d5d9ad
wukunguang <619749894@qq.com>
2016-03-02 22:12:51 +0800

    备份。

---
+++ app/src/main/java/com/sky31/gonggong/view/activity/PublishSwzlActivity.java
@@ -0,0 +1,16 @@
+package com.sky31.gonggong.view.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.sky31.gonggong.R;
+
+public class PublishSwzlActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_swzl);
+    }
+
+}
