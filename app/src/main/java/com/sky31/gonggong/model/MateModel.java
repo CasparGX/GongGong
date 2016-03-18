707d1f4cab968405288b17920262cee3a6305779
CasparGX <630248976@qq.com>
2016-03-18 21:25:59 +0800

    Update

---
+++ app/src/main/java/com/sky31/gonggong/model/MateModel.java
@@ -0,0 +1,9 @@
+package com.sky31.gonggong.model;
+
+/**
+ * Created by root on 16-3-18.
+ * 查询校友信息
+ */
+public class MateModel {
+
+}
--- app/src/main/java/com/sky31/gonggong/module/main/fragment/SecondFragment.java
+++ app/src/main/java/com/sky31/gonggong/module/main/fragment/SecondFragment.java
@@ -1,19 +1,29 @@
 package com.sky31.gonggong.module.main.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.sky31.gonggong.R;
+import com.sky31.gonggong.module.search_mate.SearchMateActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SecondFragment extends Fragment {
 
+
+    @Bind(R.id.btn_cha)
+    Button btnCha;
 
     public SecondFragment() {
         // Required empty public constructor
@@ -24,7 +34,22 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    //查水表按钮
+    @OnClick(R.id.btn_cha)
+    void onClickChaShuiBiao(View view) {
+        Intent intent = new Intent();
+        intent.setClass(getActivity(), SearchMateActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
