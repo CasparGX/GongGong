fdc17b60cab2eab645f33a81cb90030303c21e36
CasparGX <630248976@qq.com>
2016-03-27 21:52:49 +0800

    Update

--- app/src/main/java/com/sky31/gonggong/widget/InputPassPopupwindow.java
+++ app/src/main/java/com/sky31/gonggong/widget/InputPassPopupwindow.java
@@ -1,41 +1,51 @@
 package com.sky31.gonggong.widget;
 
+import android.app.Activity;
 import android.content.Context;
-import android.view.Gravity;
+import android.support.v7.widget.AppCompatEditText;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.TextView;
 
+import com.gc.materialdesign.views.ButtonRectangle;
 import com.sky31.gonggong.R;
-import com.sky31.gonggong.config.Constants;
 
 import static com.sky31.gonggong.config.CommonFunction.backgroundAlpha;
 
 /**
  * Created by root on 16-3-27.
  */
-public class InputPassPopupwindow extends PopupWindow {
-    public InputPassPopupwindow(Context context) {
-        super(context);
-        View contentView = LayoutInflater.from(context).inflate(
-                R.layout.popupwindow_xtu_net_info, null);
-        TextView xtuNetworkPackagex = (TextView) contentView.findViewById(R.id.xtu_network_packagex);
-        TextView xtuNetworkStatus = (TextView) contentView.findViewById(R.id.xtu_network_status);
-        TextView xtuNetworkNextStatementDate = (TextView) contentView.findViewById(R.id.xtu_network_next_statement_date);
-        TextView xtuNetworkBalance = (TextView) contentView.findViewById(R.id.xtu_network_balance);
-        xtuNetworkPackagex.setText(resources.getString(R.string.xtu_network_packagex) + aCache.getAsString(Constants.Key.CAMPUS_NETWORK_PACKAGEX));
-        xtuNetworkStatus.setText(resources.getString(R.string.xtu_network_status) + aCache.getAsString(Constants.Key.CAMPUS_NETWORK_STATUS));
-        xtuNetworkNextStatementDate.setText(resources.getString(R.string.xtu_network_next_statement_date) + aCache.getAsString(Constants.Key.CAMPUS_NETWORK_NEXT_STATEMENT_DATE));
-        xtuNetworkBalance.setText(resources.getString(R.string.xtu_network_balance) + aCache.getAsString(Constants.Key.CAMPUS_NETWORK_BALANCE));
+public class InputPassPopupwindow extends PopupWindow implements View.OnClickListener {
+    AppCompatEditText etInputPassword;
+    ButtonRectangle btnConfirm;
+    Context context;
+    Activity activity;
 
+    @Override
+    public void showAtLocation(View parent, int gravity, int x, int y) {
+        super.showAtLocation(parent, gravity, x, y);
+        backgroundAlpha(0.5f, activity);
+    }
+
+    public InputPassPopupwindow(Context context, final Activity activity) {
+        super(context);
+        this.context = context;
+        this.activity = activity;
+    }
+
+    public PopupWindow getPopupWindow(String title) {
+        View contentView = LayoutInflater.from(context).inflate(
+                R.layout.popupwindow_input_password, null);
+        etInputPassword = (AppCompatEditText) contentView.findViewById(R.id.et_input_password);
+        etInputPassword.setHint(title + context.getResources().getString(R.string.password));
+        btnConfirm = (ButtonRectangle) contentView.findViewById(R.id.btn_confirm);
+        btnConfirm.setOnClickListener(this);
         final PopupWindow popupWindow = new PopupWindow(contentView,
                 LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, true);
 
         popupWindow.setTouchable(true);
-        backgroundAlpha(0.5f, MainActivity.this);
         popupWindow.setTouchInterceptor(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -48,19 +58,22 @@ public class InputPassPopupwindow extends PopupWindow {
         popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
             public void onDismiss() {
-                backgroundAlpha(1f, MainActivity.this);
+                backgroundAlpha(1f, activity);
             }
         });
 
         // 如果不设置PopupWindow的背景，无论是点击外部区域还是Back键都无法dismiss弹框
         // 我觉得这里是API的一个bug
-        popupWindow.setBackgroundDrawable(resources.getDrawable(R.drawable.bg_default));
-
-        // 设置好参数之后再show
-        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        popupWindow.setBackgroundDrawable(context.getResources().getDrawable(R.drawable.bg_default));
+        return popupWindow;
     }
 
-    public void setTitle(String title) {
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_confirm:
 
+                break;
+        }
     }
 }
