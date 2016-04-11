234d2e19aa18f5a1691813dae2b924934d55b1af
CasparGX <630248976@qq.com>
2016-04-11 22:12:00 +0800

    Update

---
+++ app/src/main/java/com/sky31/gonggong/model/HolidayNextModel.java
@@ -0,0 +1,97 @@
+package com.sky31.gonggong.model;
+
+/**
+ * Created by root on 16-4-11.
+ */
+public class HolidayNextModel {
+
+    /**
+     * code : 0
+     * msg : OK
+     * data : {"name":"清明节","start_date":"2016-04-02","end_date":"2016-04-04","total_days":3,"interval":11}
+     */
+
+    private int code;
+    private String msg;
+    /**
+     * name : 清明节
+     * start_date : 2016-04-02
+     * end_date : 2016-04-04
+     * total_days : 3
+     * interval : 11
+     */
+
+    private DataEntity data;
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public void setData(DataEntity data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public DataEntity getData() {
+        return data;
+    }
+
+    public static class DataEntity {
+        private String name;
+        private String start_date;
+        private String end_date;
+        private int total_days;
+        private int interval;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setStart_date(String start_date) {
+            this.start_date = start_date;
+        }
+
+        public void setEnd_date(String end_date) {
+            this.end_date = end_date;
+        }
+
+        public void setTotal_days(int total_days) {
+            this.total_days = total_days;
+        }
+
+        public void setInterval(int interval) {
+            this.interval = interval;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getStart_date() {
+            return start_date;
+        }
+
+        public String getEnd_date() {
+            return end_date;
+        }
+
+        public int getTotal_days() {
+            return total_days;
+        }
+
+        public int getInterval() {
+            return interval;
+        }
+    }
+}
