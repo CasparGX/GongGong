8ce90f5ea7d8770d63b023d52580706f5bf993e8
wukunguang <619749894@qq.com>
2016-04-20 20:52:35 +0800

    备份

---
+++ app/src/main/java/com/sky31/gonggong/module/swzl/SwzlDetailActivity.java
@@ -0,0 +1,13 @@
+package com.sky31.gonggong.module.swzl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SwzlDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swzl_detail);
+    }
+}
