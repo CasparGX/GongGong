300b05144c478013c7aa9e3a9fa56d2e5a82ee40
CasparGX <630248976@qq.com>
2016-04-27 22:20:52 +0800

    Update

--- app/src/main/java/com/sky31/gonggong/module/grade/GradeActivity.java
+++ app/src/main/java/com/sky31/gonggong/module/grade/GradeActivity.java
@@ -4,11 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.rey.material.widget.ProgressView;
 import com.sky31.gonggong.R;
+import com.sky31.gonggong.base.BaseActivity;
 import com.sky31.gonggong.module.grade.details.DetailsFragment;
 import com.sky31.gonggong.module.grade.rank.RankFragment;
 import com.sky31.gonggong.module.grade.report.GradeReportFragment;
@@ -20,7 +21,7 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class GradeActivity extends AppCompatActivity {
+public class GradeActivity extends BaseActivity {
 
     @Bind(R.id.pv_refresh)
     ProgressView pvRefresh;
@@ -55,5 +56,21 @@ public class GradeActivity extends AppCompatActivity {
         //TabLayout
         tabLayoutGrade.setupWithViewPager(vpGrade);
         tabLayoutGrade.setTabMode(TabLayout.MODE_SCROLLABLE);
+    }
+
+    @Override
+    protected void onTitleChanged(CharSequence title, int color) {
+        super.onTitleChanged(title, color);
+        //设置toolbar返回按钮
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        //设置toolbar标题
+        toolbar.setTitle(title);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
     }
 }
