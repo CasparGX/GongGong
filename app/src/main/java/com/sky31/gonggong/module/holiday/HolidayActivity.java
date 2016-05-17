968de86dcf7a4c80e2cef71dbec5cabb8ca6b8eb
CasparGX <630248976@qq.com>
2016-05-17 21:40:20 +0800

    Update

--- app/src/main/java/com/sky31/gonggong/module/holiday/HolidayActivity.java
+++ app/src/main/java/com/sky31/gonggong/module/holiday/HolidayActivity.java
@@ -1,15 +1,16 @@
 package com.sky31.gonggong.module.holiday;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import com.sky31.gonggong.R;
+import com.sky31.gonggong.base.BaseActivity;
 
-public class HolidayActivity extends Activity {
+public class HolidayActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_holiday);
+
     }
 }
